c0f9f41b215b9aade038c3a979fc9620843d069f
MikaelaStambuk <mstambuk@miuandes.cl>
2024-04-22 13:51:11 -0400

    3 commit lab 07

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,5 @@
 class Post < ApplicationRecord
     belongs_to: user 
-end
+    has_many :post_tags, dependent: :destroy
+    has_many :tags, through: :post_tags     
+end 
--- db/migrate/20240422172045_add_user_idto_posts.rb
+++ db/migrate/20240422172045_add_user_idto_posts.rb
@@ -2,6 +2,8 @@ class AddUserIdtoPosts < ActiveRecord::Migration[7.1]
   def change
     change_table :posts do |t|
       t.belongs_to :user
+
+      t.timestamps
     end
   end
 end
