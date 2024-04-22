ee0c155b366d6f2f54be75bc7af25c89184cf7b6
MikaelaStambuk <mstambuk@miuandes.cl>
2024-04-22 14:38:51 -0400

    5 commit lab 07

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,4 +4,18 @@ class Post < ApplicationRecord
     has_many :tags, through: :post_tags     
     belongs_to :parent_post, class_name: 'Post', optional: true
     has_many :child_posts, class_name: 'Post', foreign_key: 'parent_post_id', dependent: :destroy
+
+    validates :title, presence: true
+    validates :content, presence: true
+    validates :user_id, presence: true
+    validates :answers_count, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+    validates :likes_count, numericality: { greater_than_or_equal_to: 0, only_integer: true }
+
+    before_validation :set_default_published_at, on: :create
+
+    private
+
+    def set_default_published_at
+        self.published_at ||= Time.zone.now
+    end
 end 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,14 @@
 class User < ApplicationRecord
-    has_many :posts
+    has_many :posts, dependent: :destroy 
+
+    validates :name, presence: true
+    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :password, presence: true, length: { minimum: 6 }
+
+    before_save :downcase_email
+    private
+
+    def downcase_email
+        self.email.downcase!
+    end
 end
